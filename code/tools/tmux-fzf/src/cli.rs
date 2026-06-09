e7a050823125d35c636c59413e57af6ff1889cf0
nguyenvukhang <brew4k@gmail.com>
2026-06-08 23:11:20 -0400

    Import tmux-fzf

---
+++ code/tools/tmux-fzf/src/cli.rs
@@ -0,0 +1,35 @@
+//! All interactions with clap.rs.
+
+pub(crate) use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, about, long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Option<CliCommand>,
+
+    /// Attaches to this target when no command is specified.
+    pub attach_target: Option<String>,
+}
+
+#[derive(clap::Subcommand)]
+pub enum CliCommand {
+    /// List tmux sessions.
+    #[command(alias = "ls")]
+    List,
+
+    /// Attaches to a tmux session.
+    #[command(alias = "a")]
+    Attach {
+        #[arg(short = 't')]
+        target: String,
+    },
+
+    /// Detaches from the current tmux session.
+    #[command(alias = "d")]
+    Detach,
+
+    /// Kills a tmux session. Specify a target or pick one with fzf.
+    #[command(alias = "k")]
+    Kill { target_session: Option<String> },
+}
---
+++ code/tools/tmux-fzf/src/main.rs
@@ -0,0 +1,70 @@
+mod cli;
+mod tmux;
+
+use cli::{Cli, CliCommand, Parser};
+use tmux::send;
+
+use std::io;
+use std::io::Write;
+use std::process::{Child, Command, ExitCode, Stdio};
+
+fn spawn_fzf() -> io::Result<Child> {
+    let mut fzf = Command::new("fzf");
+
+    // If there is no match for the initial query (--query),
+    // do not start interactive finder and exit immediately
+    fzf.arg("-0");
+
+    fzf.stdin(Stdio::piped()).stdout(Stdio::piped()).spawn()
+}
+
+fn handle_command(command: CliCommand) -> io::Result<ExitCode> {
+    match command {
+        CliCommand::List => send(tmux::ls()),
+        CliCommand::Attach { target } => tmux::attach(&target),
+        CliCommand::Detach => tmux::detach(),
+        CliCommand::Kill { target_session } => {
+            if let Some(v) = target_session {
+                return tmux::kill(&v);
+            }
+            let tmux_ls = tmux::ls().stdout(Stdio::piped()).output()?;
+            let mut fzf = spawn_fzf()?;
+            fzf.stdin.as_mut().unwrap().write_all(&tmux_ls.stdout)?;
+            let fzf = fzf.wait_with_output()?;
+            if !fzf.status.success() {
+                return Ok(ExitCode::FAILURE); // Nothing is selected with fzf.
+            };
+            let selected_line = core::str::from_utf8(&fzf.stdout).unwrap();
+            let target_session = selected_line.split_once(':').unwrap().0;
+            tmux::kill(target_session)
+        }
+    }
+}
+
+fn handle_empty() -> io::Result<ExitCode> {
+    let tmux_ls = tmux::ls().stdout(Stdio::piped()).output()?;
+    if tmux_ls.stdout.is_empty() {
+        return send(tmux::new_session("0"));
+    }
+    let mut fzf = spawn_fzf()?;
+    fzf.stdin.as_mut().unwrap().write_all(&tmux_ls.stdout)?;
+    let fzf = fzf.wait_with_output()?;
+    if !fzf.status.success() {
+        return Ok(ExitCode::FAILURE); // Nothing is selected with fzf.
+    };
+    let selected_line = core::str::from_utf8(&fzf.stdout).unwrap();
+    let target_session = selected_line.split_once(':').unwrap().0;
+    tmux::attach(target_session)
+}
+
+fn main() -> io::Result<ExitCode> {
+    let cli = Cli::parse();
+    if let Some(command) = cli.command {
+        return handle_command(command);
+    }
+    let Some(target) = cli.attach_target else { return handle_empty() };
+    if !tmux::has_session(&target)? {
+        tmux::new_detached_session(&target).spawn()?.wait()?;
+    }
+    tmux::attach(&target)
+}
