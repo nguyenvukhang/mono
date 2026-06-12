74bacde79390b94235783c861534ced3908fabcd
nguyenvukhang <brew4k@gmail.com>
2026-06-12 17:35:21 +0800

    Fix zls installation instructions

--- code/docs/debian13-setup/src/targets/zls.rs
+++ code/docs/debian13-setup/src/targets/zls.rs
@@ -9,25 +9,28 @@ pub struct Zls {}
 impl Target for Zls {
     fn install(&self) {
         const SIGNATURE_FILE: &str = "/tmp/.minisig";
+        let tarfile = "/tmp/zls.tar.xz";
 
         let destination = dirs::HOME_DIR.join(".local/bin/zls");
 
         let _ = fs::remove_file(&destination);
 
-        utils::curl(&destination, pins::zls::SOURCE_URL);
+        utils::curl(tarfile, pins::zls::SOURCE_URL);
         utils::curl(SIGNATURE_FILE, &format!("{}.minisig", pins::zls::SOURCE_URL));
         sh!(
             "minisign",
             "-Vm",
-            &destination,
+            tarfile,
             "-P",
             pins::zls::MINISIGN_SIGNATURE,
             "-x",
             SIGNATURE_FILE
         );
 
+        sh!("tar", "-xvf", tarfile, "-C", destination.parent().unwrap(), "zls");
         sh!("chmod", "u+x", &destination);
 
+        let _ = fs::remove_file(tarfile);
         let _ = fs::remove_file(SIGNATURE_FILE);
     }
 }
